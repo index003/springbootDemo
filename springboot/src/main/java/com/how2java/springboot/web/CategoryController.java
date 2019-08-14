a67feff15ed396ff252818efb9c695388a6bbe5c
Administrator <Administrator@PC-20190621001>
2019-08-14 17:55:27 +0800

    JPA(Java Persistence API)是Sun官方提出的Java持久化规范，用来方便大家操作数据库。 真正干活的可能是Hibernate,TopLink等等实现了JPA规范的不同厂商,默认是Hibernate。 http://127.0.0.1:8080/listCategory

---
+++ springboot/src/main/java/com/how2java/springboot/web/CategoryController.java
@@ -0,0 +1,31 @@
+package com.how2java.springboot.web;
+import java.util.List;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+import com.how2java.springboot.dao.CategoryDAO;
+import com.how2java.springboot.pojo.Category;
+  
+/*
+1. 接受listCategory映射
+2. 然后获取所有的分类数据
+3. 接着放入Model中
+4. 跳转到listCategory.jsp中
+ */
+@Controller
+public class CategoryController {
+    @Autowired CategoryDAO categoryDAO;
+     
+    @RequestMapping("/listCategory")
+    public String listCategory(Model m) throws Exception {
+        List<Category> cs=categoryDAO.findAll();
+         
+        m.addAttribute("cs", cs);
+         
+        return "listCategory";
+    }
+     
+}
