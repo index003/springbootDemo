b2a6ff6e3a18c336b617bb8ffbf76a1ba1806090
Administrator <Administrator@PC-20190621001>
2019-08-16 15:44:51 +0800

    Restful 风格 http://127.0.0.1:8080/categories

--- springboot/src/main/java/com/how2java/springboot/web/CategoryController.java
+++ springboot/src/main/java/com/how2java/springboot/web/CategoryController.java
@@ -6,7 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
  
 import com.how2java.springboot.dao.CategoryDAO;
@@ -16,43 +20,33 @@ import com.how2java.springboot.pojo.Category;
 public class CategoryController {
     @Autowired CategoryDAO categoryDAO;
      
-    @RequestMapping("/listCategory")
-     
+    @GetMapping("/categories")
     public String listCategory(Model m,@RequestParam(value = "start", defaultValue = "0") int start,@RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
         start = start<0?0:start;
-         
         Sort sort = new Sort(Sort.Direction.DESC, "id");
         Pageable pageable = new PageRequest(start, size, sort);
         Page<Category> page =categoryDAO.findAll(pageable);
-         
-        System.out.println(page.getNumber());
-        System.out.println(page.getNumberOfElements());
-        System.out.println(page.getSize());
-        System.out.println(page.getTotalElements());
-        System.out.println(page.getTotalPages());
-         
         m.addAttribute("page", page);
-         
         return "listCategory";
     }
  
-    @RequestMapping("/addCategory")
+    @PostMapping("/categories")
     public String addCategory(Category c) throws Exception {
         categoryDAO.save(c);
-        return "redirect:listCategory";
+        return "redirect:/categories";
     }
-    @RequestMapping("/deleteCategory")
+    @DeleteMapping("/categories/{id}")
     public String deleteCategory(Category c) throws Exception {
         categoryDAO.delete(c);
-        return "redirect:listCategory";
+        return "redirect:/categories";
     }
-    @RequestMapping("/updateCategory")
+    @PutMapping("/categories/{id}")
     public String updateCategory(Category c) throws Exception {
         categoryDAO.save(c);
-        return "redirect:listCategory";
+        return "redirect:/categories";
     }
-    @RequestMapping("/editCategory")
-    public String ediitCategory(int id,Model m) throws Exception {
+    @GetMapping("/categories/{id}")
+    public String getCategory(@PathVariable("id") int id,Model m) throws Exception {
         Category c= categoryDAO.getOne(id);
         m.addAttribute("c", c);
         return "editCategory";
