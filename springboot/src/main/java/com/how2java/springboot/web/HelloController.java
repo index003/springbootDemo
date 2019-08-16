7b35e53a366fde86280b917647c656e58231b6ba
Administrator <Administrator@PC-20190621001>
2019-08-16 18:50:31 +0800

    thymeleaf 跟 JSP 一样，就是运行之后，就得到纯 HTML了。  区别在与，不运行之前， thymeleaf 也是 纯 html http://127.0.0.1:8080/thymeleaf/hello

--- springboot/src/main/java/com/how2java/springboot/web/HelloController.java
+++ springboot/src/main/java/com/how2java/springboot/web/HelloController.java
@@ -1,7 +1,4 @@
 package com.how2java.springboot.web;
-import java.text.DateFormat;
-import java.util.Date;
- 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
   
     @RequestMapping("/hello")
-    public String hello(Model m) throws Exception{
-        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
-        if(true){
-            throw new Exception("some exception");
-        }
+    public String hello(Model m) {
+        m.addAttribute("name", "thymeleaf");
         return "hello";
-        
     }
 }
