92d0bd6ffbb078968ccdcd6e67216c38de6c06c9
Administrator <Administrator@PC-20190621001>
2019-08-16 10:49:40 +0800

    条件查询，执行TestJPA

--- springboot/src/main/java/com/how2java/springboot/dao/CategoryDAO.java
+++ springboot/src/main/java/com/how2java/springboot/dao/CategoryDAO.java
@@ -1,5 +1,7 @@
 package com.how2java.springboot.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.how2java.springboot.pojo.Category;
@@ -12,4 +14,7 @@ JpaRepository 这个父接口，就提供了CRUD, 分页等等一系列的查询
 
 public interface CategoryDAO extends JpaRepository<Category,Integer>{
 
+    public List<Category> findByName(String name);
+    
+    public List<Category> findByNameLikeAndIdGreaterThanOrderByNameAsc(String name, int id);
 }
