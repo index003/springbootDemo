d5e1015ae2c9cf1f9e690941dd4b7e517397cf8c
Administrator <Administrator@PC-20190621001>
2019-08-19 16:57:27 +0800

    使用#dates这个内置工具进行格式化日期 http://127.0.0.1:8080/thymeleaf/test

--- springboot/src/main/java/com/how2java/springboot/web/TestController.java
+++ springboot/src/main/java/com/how2java/springboot/web/TestController.java
@@ -17,6 +17,20 @@ public class TestController {
         String htmlContent = "<p style='color:red'> 红色文字</p>";
         Product currentProduct =new Product(5,"product e", 200);
         boolean testBoolean = true;
+        
+        Date now = new Date();
+        
+        List<Product> ps = new ArrayList<>();
+        ps.add(new Product(1,"product a", 50));
+        ps.add(new Product(2,"product b", 100));
+        ps.add(new Product(3,"product c", 150));
+        ps.add(new Product(4,"product d", 200));
+        ps.add(currentProduct);
+        ps.add(new Product(6,"product f", 200));
+        ps.add(new Product(7,"product g", 200));       
+         
+        m.addAttribute("now", now);
+        m.addAttribute("ps", ps);
          
         m.addAttribute("htmlContent", htmlContent);
         m.addAttribute("currentProduct", currentProduct);
